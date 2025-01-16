17a59101582329e22405c125d2d8540487792578
Juhee <hero3j@gmail.com>
2025-01-16 23:41:25 +0900

    의상고르기

---
+++ level2/42578.java
@@ -0,0 +1,16 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/42578
+
+import java.util.HashMap;
+class Solution {
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        HashMap<String,Integer> map = new HashMap<>();
+        for(String[] cloth : clothes){
+            map.put(cloth[1], map.getOrDefault(cloth[1], 0)+1);
+        }
+        for(String key : map.keySet()){
+            answer=answer*(map.get(key)+1);
+        }
+        return answer-1;
+    }
+}
