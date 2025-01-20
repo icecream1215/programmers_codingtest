72b6fc5feea493adbb7caa57135405b267c759c8
Juhee <hero3j@gmail.com>
2025-01-20 22:15:48 +0900

    연속부분수열 합의 개수(원형수열)

---
+++ level2/131701.java
@@ -0,0 +1,19 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/131701
+
+import java.util.HashSet;
+class Solution {
+    public int solution(int[] elements) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int i = 1; i <= elements.length; i++){
+            for(int j = 0; j < elements.length; j++){
+                int sum = 0;
+                for(int k=j; k < j+i; k++){
+                    sum += elements[(k%elements.length)];
+                }
+                set.add(sum);
+            }
+        }
+        return set.size();
+    }
+}
