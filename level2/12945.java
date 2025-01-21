e81789a85f9ad778dc9e2c2fadc6079bcaa49c54
Juhee <hero3j@gmail.com>
2025-01-21 15:59:28 +0900

    피보나치수열

---
+++ level2/12945.java
@@ -0,0 +1,13 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/12945
+
+class Solution {
+    public int solution(int n) {
+        int[] arr = new int[n+1];
+        arr[0] = 0;
+        arr[1] = 1;
+        for(int i=2; i < arr.length; i++){
+            arr[i] = (arr[i-2] + arr[i-1])%1234567;
+        }
+        return arr[n];
+    }
+}
